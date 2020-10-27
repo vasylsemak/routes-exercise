85f3b8e4a01d18abde4e7a016c74b72f4b921f88
Vasyl Semak <vasylsmk1401@gmail.com>
2020-10-26 21:33:40 -0400

    fix bug: write return before res.sendStatus(404), so rest of code not running

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ const { listPeople, list, add, complete, remove } = require('../models/todos');
 router.get('/', (req, res, next) => {
   try {
     const people = listPeople();
-    if(!people) res.sendStatus(404);
+    // return to ensure the rest of the code is not run
+    if(!people) return res.sendStatus(404);
     res.status(200).json(people);
   } catch (error) {
     next(error);
@@ -22,8 +23,8 @@ router
     try {
       let userTasks = list(req.params.name);
 
-      if(userTasks === undefined) {
-        res.status(404).send("No user or tasks!");
+      if(!userTasks) {
+        return res.status(404).send("No user or tasks!");
       }
       // Check for req.query object:  /users/billy/tasks?status=complete
       if(req.query.status === 'complete') {
@@ -40,6 +41,7 @@ router
   })
   .post('/:name/tasks', (req, res, next) => {
     try {
+      if(req.body.content === '') return res.sendStatus(400);
       res.status(201).json(add(req.params.name, req.body));
     } catch (error) {
       next(error);
@@ -51,7 +53,7 @@ router
   .put('/:name/tasks/:idx', (req, res, next) => {
     try {
       const afterComplete = complete(req.params.name, req.params.idx);
-      if(!afterComplete) res.sendStatus(404);
+      if(!afterComplete) return res.sendStatus(404);
       res.status(200).json(afterComplete);
     } catch (error) {
       next(error);
@@ -60,7 +62,7 @@ router
   .delete('/:name/tasks/:idx', (req, res, next) => {
     try {
       const afterDelete = remove(req.params.name, req.params.idx);
-      if(!afterDelete) res.sendStatus(404);
+      if(!afterDelete) return res.sendStatus(404);
       res.status(204).send(afterDelete);
     } catch (error) {
       next(error);
